1be6b88b96c71386a1012310fca6e6775c9bbbb1
Mike Slaby <mikeslaby3@gmail.com>
2019-05-29 13:41:28 -0700

    Set up server with express and path npm packages required

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
+
