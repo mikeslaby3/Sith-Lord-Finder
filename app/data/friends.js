583f41d06ff354b0a3b5493d6068a02e7d1da296
Mike Slaby <mikeslaby3@gmail.com>
2019-05-29 13:53:51 -0700

    Created array of sith character objects that have dummy survey scores for testing

---
+++ app/data/friends.js
@@ -0,0 +1,51 @@
+let sith = 
+[
+    {
+        "name": "Darth Vader",
+        "photo": 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwillMfHzcHiAhVCsp4KHV_cCP4QjRx6BAgBEAU&url=http%3A%2F%2Fdadfeelings.com%2Fepisode%2F1&psig=AOvVaw3PoNpuirN1Es1rfV6R8Khc&ust=1559249019270113',
+        "scores": [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        "name": "Darth Maul",
+        "photo": 'https://www.google.com/?sa=i=images&cd=&ved=2ahUKEwishO2XzsHiAhXQ6Z4KHd3CB5EQjRx6BAgBEAU&url=https%3A%2F%2Fwww.businessinsider.com%2F15-reasons-darth-maul-is-a-better-star-wars-villain-than-darth-vader-2017-8&psig=AOvVaw3kMyVkZUCfbGiyB3E2RarX&ust=1559249220317334',
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Darth Sideous",
+        "photo": 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwicjebDzsHiAhVDjp4KHfGvB80QjRx6BAgBEAU&url=https%3A%2F%2Fstarwars.fandom.com%2Fwiki%2FDarth_Sidious&psig=AOvVaw3BZR8nxVPBmpMjKRYLWFUA&ust=1559249319046675',
+        "scores": [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+]
