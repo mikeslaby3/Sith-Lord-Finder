825e311950aafa1b2ec1340094cc862983b627f2
Mike Slaby <mikeslaby3@gmail.com>
2019-06-02 14:23:01 -0700

    Changed answers of default Sith Lords to be more realistic

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,15 +4,15 @@ let sith =
         "name": "Darth Vader",
         "photo": 'https://cdn.vox-cdn.com/thumbor/jhj4RKFtuxibuv2nSPL2nl1Y0Tk=/0x0:612x306/920x613/filters:focal(231x79:327x175):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/52403587/vaderrogueone.0.jpeg',
         "scores": [
+            3,
+            2,
+            3,
             5,
             5,
+            2,
+            4,
             5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
+            4,
             5
         ]
     },
@@ -20,32 +20,32 @@ let sith =
         "name": "Darth Maul",
         "photo": 'https://www.liveabout.com/thmb/T-foF53EdYnEDIHHnSO3HtzzQ9w=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/EP1-IA-48553_R_8x10-56a83a203df78cf7729d27de.jpg',
         "scores": [
+            5,
             1,
             1,
             1,
+            2,
             1,
             1,
-            1,
-            1,
-            1,
-            1,
-            1
+            3,
+            5,
+            3
         ]
     },
     {
         "name": "Darth Sideous",
         "photo": 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/palpatine-2-1539176362.jpg?crop=0.668xw:1.00xh;0.0969xw,0&resize=768:*',
         "scores": [
+            2,
+            5,
+            5,
+            1,
             3,
-            3,
-            3,
-            3,
-            3,
-            3,
-            3,
-            3,
-            3,
-            3
+            5,
+            5,
+            5,
+            4,
+            5
         ]
     },
 ]
