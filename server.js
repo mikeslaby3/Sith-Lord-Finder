c59ceaf93df0050c2307ac8a8de5e4b22038ffcc
Mike Slaby <mikeslaby3@gmail.com>
2019-06-02 13:49:21 -0700

    Edited relative paths of route files to fix bug

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-require('./routing/apiRoutes')(app);
-require('./routing/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
